2bd9450e02d3cc0e7e0dff607575cbfe6866187c
BugHunter-X <askzenai@gmail.com>
2024-12-15 04:18:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,11 @@
+```javascript
+//This is the wrong way to use the Tailwindcss classes
+<div class="bg-red-500 p-4 text-white">
+  <p>This is a paragraph.</p>
+</div>
+
+//This is the correct way to use the Tailwindcss classes
+<div class="bg-red-500 p-4 text-white">
+  <p>This is a paragraph.</p>
+</div>
+```
